d758705e60756caffab615af4bab419827d4af2b
“tmcamillo” <imeio.tmc@gmail.com>
2022-11-28 17:47:28 +0100

    fixed deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-/// <reference types="vitest" />
 /// <reference types="vite/client" />
 
 import { defineConfig } from 'vite';
@@ -7,9 +6,4 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./src/setupTest.ts'],
-  },
 });
